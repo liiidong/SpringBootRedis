6b450dad7c1780601a3fe3aa12d13cb3ee806465
lidong290 <lidong290@crc.com.hk>
2019-11-18 17:16:50 +0800

    Base64加解密工具
    Base64加解密工具

---
+++ learn-redis/src/test/java/com/enough/learn/learn/redis/CodecTest.java
@@ -0,0 +1,24 @@
+package com.enough.learn.learn.redis;
+
+import com.enough.learn.learn.redis.utils.CodecUtils;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @program: learn-redis
+ * @description: 加密解密测试
+ * @author: lidong
+ * @create: 2019/11/18
+ */
+public class CodecTest {
+
+    @Test
+    public void codecTest(){
+        String str = "helloword!";
+        //加密
+        System.out.println(CodecUtils.encode(str));
+        //判断
+        System.out.println("是否加密" + CodecUtils.isBase64(CodecUtils.encode(str)));
+        //解密
+        System.out.println(CodecUtils.decode(CodecUtils.encode(str)));
+    }
+}
